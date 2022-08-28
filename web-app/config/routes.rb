6e6f55ba1a49d5416004752de2e0e15768beb978
Gustavo Carvalho <gsc13@tuta.io>
2022-08-28 16:33:13 -0300

    Generate order and address model

--- web-app/config/routes.rb
+++ web-app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  scope :api do
+    resources :orders, only: [:index, :create, :update, :show]
+  end
+  
   root 'root#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
