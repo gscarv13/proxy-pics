c9cb5171ae4b2b3cba288cfb8ab2da476c0a2e1f
Gustavo Carvalho <gsc13@tuta.io>
2022-09-02 11:06:15 -0300

    Add constants example to mobile app

---
+++ mobile-app/helpers/constants.example.js
@@ -0,0 +1,6 @@
+// Update BASE URL
+export const BASE_URL = 'https://3ba8-2804-2cac-cccc-11b3-b0cc-fee1-38cf-3.sa.ngrok.io'
+
+export const GET_ORDERS = '/api/v1/assignee/orders'
+export const PUT_ORDER = '/api/v1/assignee/order'
+export const POST_AUTHENTICATION = '/authenticate/assignee'
