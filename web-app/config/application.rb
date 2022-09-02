b054ce9e82970ddf42c925fdab9f9afbc80c599d
Gustavo Carvalho <gsc13@tuta.io>
2022-09-01 22:56:22 -0300

    Add cors gem

--- web-app/config/application.rb
+++ web-app/config/application.rb
@@ -33,5 +33,12 @@ module WebApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete,]
+      end
+    end
   end
 end
